82742c983960e307624398610ae4619c2d46d949
Stanko Dujaković <stanko.dujakovic@ingemark.com>
2022-08-20 13:52:10 +0200

    Add domain models

---
+++ shared/src/commonMain/kotlin/com/example/multiplatformapp/domain/Owner.kt
@@ -0,0 +1,7 @@
+package com.example.multiplatformapp.domain
+
+class Owner {
+    var id: String? = null
+    var login: String? = null
+    var avatarUrl: String? = null
+}
---
+++ shared/src/commonMain/kotlin/com/example/multiplatformapp/domain/Repository.kt
@@ -0,0 +1,9 @@
+package com.example.multiplatformapp.domain
+
+class Repository {
+    var id: String? = null
+    var name: String? = null
+    var owner: Owner? = null
+    var description: String? = null
+    var lastUpdate: String? = null
+}
