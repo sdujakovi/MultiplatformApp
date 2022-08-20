4836a92a6b1a87dd091275dd86adba6db96c34fc
Stanko Dujaković <stanko.dujakovic@ingemark.com>
2022-08-20 17:39:30 +0200

    Test presentation on android

--- androidApp/src/main/java/com/example/multiplatformapp/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/multiplatformapp/android/MainActivity.kt
@@ -3,9 +3,15 @@ package com.example.multiplatformapp.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import com.example.multiplatformapp.Greeting
+import com.example.multiplatformapp.presentation.GithubViewModel
 
 fun greet(): String {
     return Greeting().greeting()
@@ -16,7 +22,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Text(text = "stanko")
+                val viewModel = remember { GithubViewModel("sduj") }
+                val repos by viewModel.repositories.collectAsState()
+
+                LazyColumn {
+                    itemsIndexed(
+                        items = repos,
+                        itemContent = { _, item ->
+                            Text(text = item.name.toString())
+                        }
+                    )
+                }
             }
         }
     }
