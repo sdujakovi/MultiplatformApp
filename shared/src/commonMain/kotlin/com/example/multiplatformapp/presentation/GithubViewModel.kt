7ee65fb49a187d3389023ad4f35da5bcf021920a
Stanko Dujaković <stanko.dujakovic@ingemark.com>
2022-08-21 18:39:34 +0200

    Improve viewModel

--- shared/src/commonMain/kotlin/com/example/multiplatformapp/presentation/GithubViewModel.kt
+++ shared/src/commonMain/kotlin/com/example/multiplatformapp/presentation/GithubViewModel.kt
@@ -17,18 +17,43 @@ class GithubViewModel(private val query: String) : MainDispatcher(), KoinCompone
     private val _repositories = MutableStateFlow<List<Repository>>(emptyList())
     val repositories: StateFlow<List<Repository>> = _repositories
 
+    private val _anyUseCaseInProgress = MutableStateFlow(false)
+    val anyUseCaseInProgress: StateFlow<Boolean> = _anyUseCaseInProgress
+
+    private val _anyUseCaseFailure = MutableStateFlow(Throwable())
+    val anyUseCaseFailure: StateFlow<Throwable> = _anyUseCaseFailure
+
     init {
         getGithubRepositories(query)
     }
 
     fun getGithubRepositories(query: String) {
         coroutineScope.launch {
-            _repositories.value = githubRepository.getGithubRepositories(query)
+            try {
+                _anyUseCaseInProgress.value = true
+                _repositories.value = githubRepository.getGithubRepositories(query)
+            } catch (cause: Throwable) {
+                _anyUseCaseFailure.value = cause
+            } finally {
+                _anyUseCaseInProgress.value = false
+            }
         }
     }
 
     fun observeGithubRepositories(onChange: (List<Repository>) -> Unit) {
         repositories.onEach {
+            onChange(it)
+        }.launchIn(coroutineScope)
+    }
+
+    fun observeAnyUseCaseInProgress(onChange: (Boolean) -> Unit) {
+        anyUseCaseInProgress.onEach {
+            onChange(it)
+        }.launchIn(coroutineScope)
+    }
+
+    fun observeAnyUseCaseFailure(onChange: (Throwable) -> Unit) {
+        anyUseCaseFailure.onEach {
             onChange(it)
         }.launchIn(coroutineScope)
     }
